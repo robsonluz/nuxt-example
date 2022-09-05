29f299d1f5c0f2899d59c987988858b040976088
Robson Luz <robson.luz@gmail.com>
2022-09-05 08:59:13 -0300

    exemplo de tabela

---
+++ pages/tabela.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Coluna 1</th>
+                    <th>Coluna 2</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Dado 1</td>
+                    <td>Dado 2</td>
+                </tr>
+                <tr>
+                    <td>Dado 1</td>
+                    <td>Dado 2</td>
+                </tr>
+                <tr>
+                    <td>Dado 1</td>
+                    <td>Dado 2</td>
+                </tr>                                
+            </tbody>
+        </table>
+    </div>
+</template>
