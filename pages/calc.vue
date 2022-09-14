a00f1466d0de1ee653042ef3fc2b1a17df19c469
Robson Joao Padilha da Luz <robson.luz@gmail.com>
2022-09-14 07:49:25 -0300

    calculadora

---
+++ pages/calc.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1 class="title">
+            {{mensagem}}
+        </h1>
+        <b-button type="is-primary" @click="teste()">
+            Clique aqui
+        </b-button>
+
+        <b-field label="Mensagem">
+            <b-input v-model="mensagem"></b-input>
+        </b-field>        
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            mensagem: "Olá mundo",
+            numero1: 0,
+            numero2: 0,
+            resultado: 0
+        }
+    },
+    methods: {
+        teste() {
+            this.mensagem = "Nova mensagem";
+        },
+    }
+}
+</script>
