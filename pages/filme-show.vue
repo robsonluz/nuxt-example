71e35414aa0d70fa92b22fd6372de77607a0333e
Robson João Padilha da Luz <robson.luz@gmail.com>
2022-10-24 13:55:48 +0000

    exemplo de show

---
+++ pages/filme-show.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+    <h1 class="title">Filme</h1>
+
+    <div class="box" v-if="filme">
+      Título: {{filme.titulo}} <br> 
+      Descrição: {{filme.descricao}} <br>
+      Sinopse: {{filme.sinopse}} <br>
+  
+    </div>
+    
+  </div>
+</template>
+<script>
+export default {
+  
+  async asyncData({ $axios, query }) {
+    const filme = await $axios.$get('/api/filmes/' + query.id + '/')
+    return { filme }
+  }
+  
+}
+</script>
--- pages/filmes.vue
+++ pages/filmes.vue
@@ -4,7 +4,11 @@
     <h1 class="title">Lista de filmes</h1>
 
     <div class="box" v-for="filme in filmes" :key="filme.id">
-      Título: {{filme.titulo}} <br> 
+      Título: 
+      <router-link :to="{ path: '/filme-show', query: {id: filme.id} }">
+        {{filme.titulo}} 
+      </router-link>
+      <br> 
       Descrição: {{filme.descricao}} <br>
       Sinopse: {{filme.sinopse}} <br>
   
@@ -13,6 +17,7 @@
       </div>
 
       <img :src="filme.fotoCapa" >
+      
       
     </div>
    
