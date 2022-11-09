380c1c57832bcc736d47a930c20f236a133407bc
Robson João Padilha da Luz <robson.luz@gmail.com>
2022-11-09 00:22:10 +0000

    ajustes

--- store/index.js
+++ store/index.js
@@ -1,51 +1,55 @@
 export const state = () => ({
-    currentUser: {}
+  currentUser: {}
 })
 
 export const getters = {
-    getCurrentUser(state) {
-        return state.currentUser
-    }
+  getCurrentUser(state) {
+    return state.currentUser
+  }
 }
 
 export const mutations = {
-    currentUser(state, value) {
-        state.currentUser = value
-    },
+  currentUser(state, value) {
+    state.currentUser = value
+  },
 
-    cleanCurrentUser(state) {
-        state.currentUser = null;
-    },    
+  setCurrentUser(state, value) {
+    state.currentUser = value
+  },  
+
+  cleanCurrentUser(state) {
+    state.currentUser = null;
+  },
 }
 
 export const actions = {
-    cleanCurrentUser({ commit }) {
-        commit('cleanCurrentUser');
-    },
-    setCurrentUser({ commit }, user) {
-        commit('setCurrentUser', user);
-    },        
-    /*
-    async fetchCurrentUser({ state }) {
-        console.log('chegou aqui');
-        const currentUser = await $axios.$get('currentusuario')
-        state.currentUser = currentUser;
-        return currentUser;
-    },    
-    async nuxtServerInit({ commit }, { $axios }) {
-        console.log('buscando currentUser');
-        try {
-            const currentUser = await $axios.$get('currentusuario')
-            console.log('current user');
-            console.log(currentUser);
-            if ( currentUser != null && currentUser.id != null) {
-                commit('currentUser', currentUser)
-            }
+  cleanCurrentUser({ commit }) {
+    commit('cleanCurrentUser');
+  },
+  setCurrentUser({ commit }, user) {
+    commit('setCurrentUser', user);
+  },
+  /*
+  async fetchCurrentUser({ state }) {
+      console.log('chegou aqui');
+      const currentUser = await $axios.$get('currentusuario')
+      state.currentUser = currentUser;
+      return currentUser;
+  },    
+  async nuxtServerInit({ commit }, { $axios }) {
+      console.log('buscando currentUser');
+      try {
+          const currentUser = await $axios.$get('currentusuario')
+          console.log('current user');
+          console.log(currentUser);
+          if ( currentUser != null && currentUser.id != null) {
+              commit('currentUser', currentUser)
+          }
 
-        }catch(e) {
-           console.log(e);
-        }
-        
-    }
-    */
+      }catch(e) {
+         console.log(e);
+      }
+      
+  }
+  */
 }
